f70eab343383effadcac0d59ca224812e1040a9a
Omar <oggr99@gmail.com>
2023-11-09 22:10:42 -0700

    Upgrade packages

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ def load_user(user_id):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///blog.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 db = SQLAlchemy()
 db.init_app(app)
 
